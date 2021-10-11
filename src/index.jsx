ed41a8ace70685b1031551c9ebda8dcae01a8652
Patrik Nilsson <patriknilsson1992@gmail.com>
2021-10-11 16:53:50 +0700

    init project with Vite, React & Tailwind

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+function Application() {
+	return <div className="text-pink-500">Vite with React and Tailwind</div>
+}
+
+ReactDom.render(<Application />, document.getElementById('root'))
