2ad342e2a4855f54e31e2f752be67b72a94c4f7c
Patrik Nilsson <patriknilsson1992@gmail.com>
2021-10-11 17:17:47 +0700

    Adds eslint prettier

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 function Application() {
-	return <div className="text-pink-500">Vite with React and Tailwind</div>
+  return <div className="text-pink-500">Vite with React and Tailwind</div>
 }
 
 ReactDom.render(<Application />, document.getElementById('root'))
