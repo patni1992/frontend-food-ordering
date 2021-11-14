7e83589749e86b6b17522acb66935bcadf75d7c1
Patrik Nilsson <patriknilsson1992@gmail.com>
2021-11-14 14:09:28 +0700

    Setups svg

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const reactRefresh = require('@vitejs/plugin-react-refresh')
+const reactSvgPlugin = require('vite-plugin-react-svg')
+
 export default {
-	root: "src",
-};
+  root: 'src',
+  plugins: [
+    reactRefresh(),
+    reactSvgPlugin({
+      defaultExport: 'component',
+    }),
+  ],
+}
